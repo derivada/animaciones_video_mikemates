dc328ceb907509526ceb88749f1afe9bb729fde8
pablodiaz <derivadas@protonmail.com>
2023-08-21 14:53:18 +0200

    manim_12 part 2

--- anim/manim_12.py
+++ anim/manim_12.py
@@ -28,7 +28,18 @@ class manim_12(Scene):
         pol1_r1, pol1_r2 = Dot([-sqrt(2), 0.0, 0.0], color = GREEN), Dot([sqrt(2), 0.0, 0.0], color = GREEN)
         pol2_r1, pol2_r2 = Dot([0.6045, 0.0, 0.0], color = BLUE), Dot([1.5736, 0.0, 0.0], color = BLUE)
         pol3_r1 = Dot([-1.1673, 0.0, 0.0], color = RED)
-
-        self.play(AnimationGroup(FadeIn(pol1_r1), FadeIn(pol1_r2), FadeIn(pol2_r1), FadeIn(pol2_r2), FadeIn(pol3_r1), lag_ratio = 0.5, run_time = 2))
-        self.play(Indicate(pol1_r1), Indicate(pol1_r2), Indicate(pol2_r1), Indicate(pol2_r2), Indicate(pol3_r1), run_time = 1)
+        roots_group = VGroup(pol1_r1, pol3_r1, pol2_r1, pol1_r2, pol2_r2)
+        self.play(AnimationGroup(*[FadeIn(root) for root in roots_group], lag_ratio = 0.5, run_time = 2))
+        self.wait(2)
+        self.play(AnimationGroup(FadeOut(pol1), FadeOut(label1), FadeOut(pol2), FadeOut(label2), FadeOut(pol3), FadeOut(label3), lag_ratio = 0.2, run_time = 2))
+        self.wait(2)
+        self.play(AnimationGroup(*[Indicate(root) for root in roots_group], lag_ratio = 0.2))
+        
+        alg_pos = [-3.0, -2.0, 0.0]
+        for root in roots_group:
+            arrow = Line(start = alg_pos, end = root.get_center(), color = root.get_color())
+            self.play(FadeIn(arrow), run_time = 0.5)
+        alg = Text("Números algebraicos", t2c = {"algebraicos": YELLOW}, font_size = fs).next_to(alg_pos, DOWN/2)
+        self.wait(1)
+        self.play(DrawBorderThenFill(alg), run_time = 2)
         self.wait(3)
