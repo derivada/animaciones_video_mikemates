fc9098cfa802fe663445908d3d678daffb6d9996
derivada <derivadas@protonmail.com>
2023-08-27 17:17:01 +0200

    manim_03 v2

--- anim/manim_03.py
+++ anim/manim_03.py
@@ -1,110 +1,125 @@
 from manim import *
 from numpy import *
+from utils import *
 
-rect_h, rect_w = 1, 2
-rect_color, rect_thick, rect_fill, rect_fill_opacity = RED, DEFAULT_STROKE_WIDTH * 1.2, RED_D, 0.5
-arc_color, arc_thick, arc_alpha = GREEN, DEFAULT_STROKE_WIDTH, 0.5
-aux_color, aux_thick, aux_arc_len = BLUE, DEFAULT_STROKE_WIDTH * 0.6, PI/16
-point_color, point_thick = WHITE, DEFAULT_STROKE_WIDTH * 0.6
-cuad_color, cuad_color_highlight, cuad_thick, cuad_fill, cuad_fill_opacity = YELLOW, WHITE, DEFAULT_STROKE_WIDTH * 1.2, YELLOW_D, 0.5
-fs = 32
+fs = 60
+poly_points = [[2.0, 0.0, 0.0], [2.0, 2.5, 0.0], [-3.0, 3.0, 0.0], [-1.5, 0.5, 0.0], [-2.0, -3.0, 0.0], [3.0, -2.0, 0.0]]
+poly_color, triang_line_color = BLUE, WHITE
 
-# Sección 3.4
+
+# Sección 3.3.1-3.3.4
 class manim_03(Scene):
-    def trazar_arco_2puntos(self, center: Dot, arc : Arc, angle, run_time = 2.0, show_vect = True):
-        orig_color = center.get_color()
-        if(show_vect):
-            vect = Arrow(start = center.get_center(), end = arc.get_start(), buff = 0, color = aux_color, 
-                         stroke_width = aux_thick, max_tip_length_to_length_ratio = 0.05, tip_shape = StealthTip, stroke_opacity = 0.5) 
-            self.play(Create(vect), center.animate(rate_func = rush_into).set_color(color = YELLOW_B), run_time = run_time/8)
-            self.play(
-                Create(arc),
-                Rotate(vect, angle, about_point = center.get_center()),
-                run_time = run_time*6/8
-            )
-            self.play(Uncreate(vect), center.animate(rate_func = rush_from).set_color(color = orig_color), run_time = run_time/8)
-        else:
-            self.play(center.animate(rate_func = rush_into).set_color(color = YELLOW_B), run_time = run_time/8)
-            self.play(
-                Create(arc),
-                Rotate(vect, angle, about_point = center.get_center()),
-                run_time = run_time*6/8
-            )
-            self.play(center.animate(rate_func = rush_from).set_color(color = orig_color), run_time = run_time/8)
-
     def construct(self):
-        # constantes para retocar más fácil
-
-        removeGroup = Group() # cosas que se quitan al final
-
-        # rectángulo
-        rect = Rectangle(rect_color, rect_h, rect_w, stroke_width = rect_thick)
-        pB = Dot([-rect_w /2, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
-        pB_label = Tex("B", font_size = fs).next_to(pB, UL / 2)
-        pC = Dot([-rect_w /2, -rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
-        pC_label = Tex("C", font_size = fs).next_to(pC, DL / 2)
-        pD = Dot([rect_w /2, -rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
-        pD_label = Tex("D", font_size = fs).next_to(pD, DR / 2)
-        pE = Dot([rect_w /2, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
-        pE_label = Tex("E", font_size = fs).next_to(pE, UR / 2)
-        self.play(Create(rect))
-        self.play(AnimationGroup(FadeIn(pB), FadeIn(pB_label), FadeIn(pC), FadeIn(pC_label), FadeIn(pD), FadeIn(pD_label), FadeIn(pE), FadeIn(pE_label), lag_ratio = 0.25))
-        removeGroup.add(pB, pB_label, pC, pC_label, pD, pD_label, pE, pE_label)
+        poly = Polygon(*poly_points, color=poly_color, stroke_width = DEFAULT_STROKE_WIDTH*1.5)
+        self.play(Create(poly), run_time = 2)
+        self.wait(3)
+        triang_line_1 = Line(poly_points[0], poly_points[4], color=triang_line_color, buff = DEFAULT_STROKE_WIDTH)
+        triang_line_2 = Line(poly_points[4], poly_points[1], color=triang_line_color, buff = DEFAULT_STROKE_WIDTH)
+        triang_line_3 = Line(poly_points[1], poly_points[3], color=triang_line_color, buff = DEFAULT_STROKE_WIDTH)
+        self.play(Create(triang_line_1))
+        self.wait(0.3)
+        self.play(Create(triang_line_2))
+        self.wait(0.3)
+        self.play(Create(triang_line_3))
         self.wait(3)
 
-        # arco DF
-        pF = Dot([rect_w /2 + rect_h, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
-        pF_label = Tex("F", font_size = fs).next_to(pF, DR / 2)
-        arc_df = ArcBetweenPoints(pD.get_center(), pF.get_center(), stroke_width = arc_thick, stroke_color = arc_color, stroke_opacity = arc_alpha)
-        self.trazar_arco_2puntos(pE, arc_df, PI/2)
-        self.play(FadeIn(pF), FadeIn(pF_label))
-        removeGroup.add(pF, pF_label, arc_df)
+        poly_group = VGroup(poly, triang_line_1, triang_line_2, triang_line_3)
+
+        triangle_1 = Polygon(poly_points[0], poly_points[5], poly_points[4], color = PURPLE_A, fill_opacity = 0.5, stroke_width = 0)
+        triangle_2 = Polygon(poly_points[0], poly_points[4], poly_points[1], color = PURPLE_B, fill_opacity = 0.5, stroke_width = 0)
+        triangle_3 = Polygon(poly_points[1], poly_points[4], poly_points[3], color = PURPLE_C, fill_opacity = 0.5, stroke_width = 0)
+        triangle_4 = Polygon(poly_points[1], poly_points[3], poly_points[2], color = PURPLE_D, fill_opacity = 0.5, stroke_width = 0)
+        triang_group = VGroup(triangle_1, triangle_2, triangle_3, triangle_4)
+        self.play(AnimationGroup(FadeIn(triangle_1), FadeIn(triangle_2), FadeIn(triangle_3), FadeIn(triangle_4), run_time = 3, lag_ratio = 0.8))
+        self.play(triangle_1.animate.shift(RIGHT * 1.5),
+                    triangle_2.animate.shift(UR),
+                    triangle_3.animate.shift(DOWN * 0.8 + LEFT*1.5),
+                    triangle_4.animate.shift(LEFT),
+                    FadeOut(poly_group, run_time =1))
         self.wait(3)
 
-        # construccion mediatriz (opcional)
-        arc_bf_up = Arc(rect_w + rect_h, PI/3 + aux_arc_len/2, -aux_arc_len, arc_center = pB.get_center(), color = aux_color, stroke_width = aux_thick)
-        arc_bf_down = Arc(rect_w + rect_h, -PI/3 + aux_arc_len/2, -aux_arc_len, arc_center = pB.get_center(), color = aux_color, stroke_width = aux_thick)
-        arc_fb_up = Arc(rect_w + rect_h, 2*PI/3 - aux_arc_len/2, aux_arc_len, arc_center = pF.get_center(), color = aux_color, stroke_width = aux_thick)
-        arc_fb_down = Arc(rect_w + rect_h, -2*PI/3 - aux_arc_len/2, aux_arc_len, arc_center = pF.get_center(), color = aux_color, stroke_width = aux_thick)
-        midline = DashedLine(start = [rect_h/2, rect_w + rect_h + 0.5, 0.0], end = [rect_h/2, - rect_w - rect_h - 0.5, 0.0], color = aux_color, stroke_width = aux_thick) 
-        pG = Dot([rect_h/2, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
-        pG_label = Tex("G", font_size = fs).next_to(pG, UP / 2)
-        self.trazar_arco_2puntos(pB, arc_bf_up, -aux_arc_len, run_time = 1.0)
-        self.trazar_arco_2puntos(pB, arc_bf_down, -aux_arc_len, run_time = 1.0)
-        self.trazar_arco_2puntos(pF, arc_fb_up, aux_arc_len, run_time = 1.0)
-        self.trazar_arco_2puntos(pF, arc_fb_down, aux_arc_len, run_time = 1.0)
+        self.play(*[triangle.animate.scale(0.5) for triangle in triang_group])
+        self.play(triang_group.animate.arrange(LEFT))
+        self.wait(3)
+        
+        for tri in triang_group:
+            vert = tri.get_vertices()
+            side_len = sqrt(abs(0.5 * (vert[0][0] * (vert[1][1]-vert[2][1]) + vert[1][0] * (vert[2][1]-vert[0][1]) + vert[2][0] * (vert[0][1] - vert[1][1]))))
+            quad = Square(side_len, color = tri.get_color(), fill_opacity = 0.5,  stroke_width = 0).move_to(tri.get_center()) 
+            self.play(Transform(tri, quad))
+            self.wait(0.5)
+        
         self.wait(1)
-        self.play(AnimationGroup(Create(midline), FadeIn(pG), FadeIn(pG_label), lag_ratio = 1))
-        self.play(FadeOut(arc_bf_up), FadeOut(arc_bf_down), FadeOut(arc_fb_up), FadeOut(arc_fb_down), FadeOut(midline)) # quitar clutter
-        removeGroup.add(pG, pG_label)
+        self.play(triang_group.animate.arrange(DOWN))
+        self.play(triang_group.animate.shift(RIGHT*4))
         self.wait(3)
 
-        # circulo G y triangulo GEH
-        sq_size = sqrt(rect_h*rect_w)
-        circle = Arc(rect_w/2 + rect_h/2, angle = 2*PI, arc_center = pG.get_center(), color = arc_color, stroke_width = arc_thick, stroke_opacity = arc_alpha)
-        pH = Dot([rect_w/2, rect_h/2 + sq_size, 0.0], color = point_color, stroke_width = point_thick)
-        pH_label = Tex("H", font_size = fs).next_to(pH, UP / 2)
-        eh = Line(pE.get_center(), pH.get_center(), color = cuad_color, stroke_width = cuad_thick)
-        self.trazar_arco_2puntos(pG, circle, 2*PI)
-        self.play(Create(eh)) 
-        self.bring_to_back(eh) # maybe needs fix
-        self.play(FadeIn(pH), FadeIn(pH_label))
-        removeGroup.add(pH, pH_label, circle)
+        pitagoras_pos = [-3.5, 3.0, 0.0]
+        pitagoras = Tex("Teorema de Pitágoras", font_size = fs).move_to(pitagoras_pos)
+        pitagoras_points = [[1.5, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]]
+        pitagoras_triang = Polygon(*pitagoras_points)
+        label_a = Tex("a").next_to(pitagoras_triang, DOWN)
+        label_b = Tex("b").next_to(pitagoras_triang, LEFT)
+        label_c = Tex("c").move_to(pitagoras_triang, RIGHT/4).shift(UL/4)
+        pitagoras_geom = VGroup(pitagoras_triang, label_a, label_b, label_c).next_to(pitagoras, DOWN).align_to(pitagoras, LEFT)
+        pitagoras_eq = Tex("$a^2 + b^2 = c^2$", font_size = fs, color = BLUE).next_to(pitagoras_geom, RIGHT*1.25).shift(UP/4)
+        self.play(DrawBorderThenFill(pitagoras), run_time = 2)
+        self.play(FadeIn(pitagoras_geom))
+        self.play(DrawBorderThenFill(pitagoras_eq), run_time = 2)
         self.wait(3)
 
-        # construir cuadrado
-        cuad = Polygon(pE.get_center(), pH.get_center(), pH.get_center() + [sq_size, 0.0, 0.0], pE.get_center() + [sq_size, 0.0, 0.0], color = cuad_color, stroke_width = cuad_thick)
-        self.play(Create(cuad))
-        self.bring_to_back(cuad) # maybe needs fix
-        self.remove(eh)
+        # down-left corner of the construction triangle
+        constr_corner = [-3.5, -2.0, 0.0]
+        # first 2
+        self.play(triang_group[0].animate.next_to(constr_corner, UL, buff = 0), triang_group[1].animate.next_to(constr_corner, DR, buff = 0), run_time = 2)
         self.wait(2)
-        self.play(Indicate(cuad, scale_factor = 1.05, run_time = 1.0, color = cuad_color_highlight))
-        self.wait(3)
+        right_p, up_p = triang_group[1].get_boundary_point(UR), triang_group[0].get_boundary_point(UR)
+        triangle_add_2 = Polygon(right_p, constr_corner, up_p, color = YELLOW_B, fill_opacity = 0.5, stroke_width = 0)
+        self.play(FadeIn(triangle_add_2))
+        hyp = right_p - up_p
+        perp = [-hyp[1], hyp[0], 0.0]
+        square_add_2 = Polygon(right_p, up_p, up_p + perp, right_p + perp, color = BLUE_B, fill_opacity = 0.5, stroke_width = 0)
+        self.play(FadeIn(square_add_2))
+        self.wait(2)
+        self.play(AnimationGroup(
+                    square_add_2.animate.rotate(angle = arctan((np.linalg.norm(up_p - constr_corner)) / np.linalg.norm((constr_corner - right_p))), about_point = right_p),
+                    FadeOut(triang_group[0]), FadeOut(triang_group[1]), FadeOut(triangle_add_2), lag_ratio = 0.2, run_time = 2))
+        
+        # next 2
+        self.play(square_add_2.animate.next_to(constr_corner, UL, buff = 0), triang_group[2].animate.next_to(constr_corner, DR, buff = 0), run_time = 2)
+        self.wait(2)
+        right_p, up_p = triang_group[2].get_boundary_point(UR), square_add_2.get_boundary_point(UR)
+        triangle_add_3 = Polygon(right_p, constr_corner, up_p, color = YELLOW_C, fill_opacity = 0.5, stroke_width = 0)
+        self.play(FadeIn(triangle_add_3))
+        hyp = right_p - up_p
+        perp = [-hyp[1], hyp[0], 0.0]
+        square_add_3 = Polygon(right_p, up_p, up_p + perp, right_p + perp, color = BLUE_C, fill_opacity = 0.5, stroke_width = 0)
+        self.play(FadeIn(square_add_3))
+        self.wait(2)
+        self.play(AnimationGroup(
+                    square_add_3.animate.rotate(angle = arctan((np.linalg.norm(up_p - constr_corner)) / np.linalg.norm((constr_corner - right_p))), about_point = right_p),
+                    FadeOut(square_add_2), FadeOut(triang_group[2]), FadeOut(triangle_add_3), lag_ratio = 0.2, run_time = 2))
+        self.wait(2)
 
-        # quitar todo excepto rect y sq, resaltar áreas y números
-        self.play(AnimationGroup(*[FadeOut(s) for s in removeGroup], lag_ratio = 0.1))
-        equal = Tex("=", font_size = fs*4)
-        finalGroup = Group(rect, equal, cuad)
-        self.play(rect.animate.set_fill(rect_fill, rect_fill_opacity), cuad.animate.set_fill(cuad_fill, cuad_fill_opacity))
-        self.play(FadeIn(equal), finalGroup.animate.arrange(direction = RIGHT, buff = 0.5))
+        # last 2
+        self.play(square_add_3.animate.next_to(constr_corner, UL, buff = 0), triang_group[3].animate.next_to(constr_corner, DR, buff = 0), run_time = 2)
+        self.wait(2)
+        right_p, up_p = triang_group[3].get_boundary_point(UR), square_add_3.get_boundary_point(UR)
+        triangle_add_4 = Polygon(right_p, constr_corner, up_p, color = YELLOW_D, fill_opacity = 0.5, stroke_width = 0)
+        self.play(FadeIn(triangle_add_4))
+        hyp = right_p - up_p
+        perp = [-hyp[1], hyp[0], 0.0]
+        square_add_4 = Polygon(right_p, up_p, up_p + perp, right_p + perp, color = BLUE_D, fill_opacity = 0.5, stroke_width = 0)
+        self.play(FadeIn(square_add_4))
+        self.wait(2)
+        self.play(AnimationGroup(
+                    square_add_4.animate.rotate(angle = arctan((np.linalg.norm(up_p - constr_corner)) / np.linalg.norm((constr_corner - right_p))), about_point = right_p),
+                    FadeOut(square_add_3), FadeOut(triang_group[3]), FadeOut(triangle_add_4), lag_ratio = 0.2, run_time = 2))
+        self.wait(2)
+
+        # done
+        poly = Polygon(*poly_points, color = BLUE_D, fill_opacity = 0.5, stroke_width = 0).move_to([3.0, 0.0, 0.0]).scale(0.5)
+        equal = Tex("=", font_size = fs*1.75)
+        self.play(AnimationGroup(FadeOut(pitagoras), FadeOut(pitagoras_geom), FadeOut(pitagoras_eq), lag_ratio = 0.2, run_time = 1))
+        self.play(AnimationGroup(square_add_4.animate.move_to([-3.0, 0.0, 0.0]), FadeIn(poly), FadeIn(equal), lag_ratio = 0.8, run_time = 3))
         self.wait(3)
---
+++ anim/manim_03_old.py
@@ -0,0 +1,110 @@
+from manim import *
+from numpy import *
+
+rect_h, rect_w = 1, 2
+rect_color, rect_thick, rect_fill, rect_fill_opacity = RED, DEFAULT_STROKE_WIDTH * 1.2, RED_D, 0.5
+arc_color, arc_thick, arc_alpha = GREEN, DEFAULT_STROKE_WIDTH, 0.5
+aux_color, aux_thick, aux_arc_len = BLUE, DEFAULT_STROKE_WIDTH * 0.6, PI/16
+point_color, point_thick = WHITE, DEFAULT_STROKE_WIDTH * 0.6
+cuad_color, cuad_color_highlight, cuad_thick, cuad_fill, cuad_fill_opacity = YELLOW, WHITE, DEFAULT_STROKE_WIDTH * 1.2, YELLOW_D, 0.5
+fs = 32
+
+# Sección 3.4
+class manim_03(Scene):
+    def trazar_arco_2puntos(self, center: Dot, arc : Arc, angle, run_time = 2.0, show_vect = True):
+        orig_color = center.get_color()
+        if(show_vect):
+            vect = Arrow(start = center.get_center(), end = arc.get_start(), buff = 0, color = aux_color, 
+                         stroke_width = aux_thick, max_tip_length_to_length_ratio = 0.05, tip_shape = StealthTip, stroke_opacity = 0.5) 
+            self.play(Create(vect), center.animate(rate_func = rush_into).set_color(color = YELLOW_B), run_time = run_time/8)
+            self.play(
+                Create(arc),
+                Rotate(vect, angle, about_point = center.get_center()),
+                run_time = run_time*6/8
+            )
+            self.play(Uncreate(vect), center.animate(rate_func = rush_from).set_color(color = orig_color), run_time = run_time/8)
+        else:
+            self.play(center.animate(rate_func = rush_into).set_color(color = YELLOW_B), run_time = run_time/8)
+            self.play(
+                Create(arc),
+                Rotate(vect, angle, about_point = center.get_center()),
+                run_time = run_time*6/8
+            )
+            self.play(center.animate(rate_func = rush_from).set_color(color = orig_color), run_time = run_time/8)
+
+    def construct(self):
+        # constantes para retocar más fácil
+
+        removeGroup = Group() # cosas que se quitan al final
+
+        # rectángulo
+        rect = Rectangle(rect_color, rect_h, rect_w, stroke_width = rect_thick)
+        pB = Dot([-rect_w /2, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
+        pB_label = Tex("B", font_size = fs).next_to(pB, UL / 2)
+        pC = Dot([-rect_w /2, -rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
+        pC_label = Tex("C", font_size = fs).next_to(pC, DL / 2)
+        pD = Dot([rect_w /2, -rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
+        pD_label = Tex("D", font_size = fs).next_to(pD, DR / 2)
+        pE = Dot([rect_w /2, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
+        pE_label = Tex("E", font_size = fs).next_to(pE, UR / 2)
+        self.play(Create(rect))
+        self.play(AnimationGroup(FadeIn(pB), FadeIn(pB_label), FadeIn(pC), FadeIn(pC_label), FadeIn(pD), FadeIn(pD_label), FadeIn(pE), FadeIn(pE_label), lag_ratio = 0.25))
+        removeGroup.add(pB, pB_label, pC, pC_label, pD, pD_label, pE, pE_label)
+        self.wait(3)
+
+        # arco DF
+        pF = Dot([rect_w /2 + rect_h, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
+        pF_label = Tex("F", font_size = fs).next_to(pF, DR / 2)
+        arc_df = ArcBetweenPoints(pD.get_center(), pF.get_center(), stroke_width = arc_thick, stroke_color = arc_color, stroke_opacity = arc_alpha)
+        self.trazar_arco_2puntos(pE, arc_df, PI/2)
+        self.play(FadeIn(pF), FadeIn(pF_label))
+        removeGroup.add(pF, pF_label, arc_df)
+        self.wait(3)
+
+        # construccion mediatriz (opcional)
+        arc_bf_up = Arc(rect_w + rect_h, PI/3 + aux_arc_len/2, -aux_arc_len, arc_center = pB.get_center(), color = aux_color, stroke_width = aux_thick)
+        arc_bf_down = Arc(rect_w + rect_h, -PI/3 + aux_arc_len/2, -aux_arc_len, arc_center = pB.get_center(), color = aux_color, stroke_width = aux_thick)
+        arc_fb_up = Arc(rect_w + rect_h, 2*PI/3 - aux_arc_len/2, aux_arc_len, arc_center = pF.get_center(), color = aux_color, stroke_width = aux_thick)
+        arc_fb_down = Arc(rect_w + rect_h, -2*PI/3 - aux_arc_len/2, aux_arc_len, arc_center = pF.get_center(), color = aux_color, stroke_width = aux_thick)
+        midline = DashedLine(start = [rect_h/2, rect_w + rect_h + 0.5, 0.0], end = [rect_h/2, - rect_w - rect_h - 0.5, 0.0], color = aux_color, stroke_width = aux_thick) 
+        pG = Dot([rect_h/2, rect_h/2, 0.0], color = point_color, stroke_width = point_thick)
+        pG_label = Tex("G", font_size = fs).next_to(pG, UP / 2)
+        self.trazar_arco_2puntos(pB, arc_bf_up, -aux_arc_len, run_time = 1.0)
+        self.trazar_arco_2puntos(pB, arc_bf_down, -aux_arc_len, run_time = 1.0)
+        self.trazar_arco_2puntos(pF, arc_fb_up, aux_arc_len, run_time = 1.0)
+        self.trazar_arco_2puntos(pF, arc_fb_down, aux_arc_len, run_time = 1.0)
+        self.wait(1)
+        self.play(AnimationGroup(Create(midline), FadeIn(pG), FadeIn(pG_label), lag_ratio = 1))
+        self.play(FadeOut(arc_bf_up), FadeOut(arc_bf_down), FadeOut(arc_fb_up), FadeOut(arc_fb_down), FadeOut(midline)) # quitar clutter
+        removeGroup.add(pG, pG_label)
+        self.wait(3)
+
+        # circulo G y triangulo GEH
+        sq_size = sqrt(rect_h*rect_w)
+        circle = Arc(rect_w/2 + rect_h/2, angle = 2*PI, arc_center = pG.get_center(), color = arc_color, stroke_width = arc_thick, stroke_opacity = arc_alpha)
+        pH = Dot([rect_w/2, rect_h/2 + sq_size, 0.0], color = point_color, stroke_width = point_thick)
+        pH_label = Tex("H", font_size = fs).next_to(pH, UP / 2)
+        eh = Line(pE.get_center(), pH.get_center(), color = cuad_color, stroke_width = cuad_thick)
+        self.trazar_arco_2puntos(pG, circle, 2*PI)
+        self.play(Create(eh)) 
+        self.bring_to_back(eh) # maybe needs fix
+        self.play(FadeIn(pH), FadeIn(pH_label))
+        removeGroup.add(pH, pH_label, circle)
+        self.wait(3)
+
+        # construir cuadrado
+        cuad = Polygon(pE.get_center(), pH.get_center(), pH.get_center() + [sq_size, 0.0, 0.0], pE.get_center() + [sq_size, 0.0, 0.0], color = cuad_color, stroke_width = cuad_thick)
+        self.play(Create(cuad))
+        self.bring_to_back(cuad) # maybe needs fix
+        self.remove(eh)
+        self.wait(2)
+        self.play(Indicate(cuad, scale_factor = 1.05, run_time = 1.0, color = cuad_color_highlight))
+        self.wait(3)
+
+        # quitar todo excepto rect y sq, resaltar áreas y números
+        self.play(AnimationGroup(*[FadeOut(s) for s in removeGroup], lag_ratio = 0.1))
+        equal = Tex("=", font_size = fs*4)
+        finalGroup = Group(rect, equal, cuad)
+        self.play(rect.animate.set_fill(rect_fill, rect_fill_opacity), cuad.animate.set_fill(cuad_fill, cuad_fill_opacity))
+        self.play(FadeIn(equal), finalGroup.animate.arrange(direction = RIGHT, buff = 0.5))
+        self.wait(3)
