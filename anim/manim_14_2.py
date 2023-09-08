9ad5d81d2cb0920a6f3db1601eaecd3d5f7380bb
xianacarrera <xiana.carrera.alonso@gmail.com>
2023-09-08 17:31:28 +0200

    Mortis Causa

--- anim/manim_14_2.py
+++ anim/manim_14_2.py
@@ -65,7 +65,7 @@ class manim_14_2(Scene):
         g = Tex(r"g(x) := f(ix)\,f(-ix)", font_size = fs*1.3).to_edge(UP).shift([3, 0, 0])
         self.play(Write(g))
 
-        g_real = Tex(r"g tiene coeficientes reales:", font_size = fs).next_to(g, DOWN * 2)
+        g_real = Tex(r"g tiene coeficientes reales", font_size = fs).next_to(g, DOWN * 2)
         g_real_proof_1_1 = MathTex(r"\overline{g(x)} =")
         g_real_proof_1_2 = MathTex(r"\overline{f(ix)f(-ix)}").next_to(g_real_proof_1_1, RIGHT)
         g_real_proof_1 = VGroup(g_real_proof_1_1, g_real_proof_1_2).next_to(g_real, DOWN)
@@ -76,7 +76,7 @@ class manim_14_2(Scene):
 
         self.play(Write(g_real))
         self.play(Write(g_real_proof_1), run_time=2)
-        self.play(Write(g_real_proof_2), run_time=2)
+        self.play(Write(g_real_proof_2), run_time=3)
         self.play(Write(g_real_proof_3), run_time=2)
         self.wait()
         self.play(AnimationGroup(
@@ -87,10 +87,73 @@ class manim_14_2(Scene):
         ))
         self.wait()
 
-        f_racional = Tex(r"f tiene coeficientes racionales", font_size = fs).next_to(g_real_proof_3, DOWN)
-        self.play(Write(f_racional),
+
+        f_rational = Tex(r"f tiene coeficientes racionales", font_size = fs).next_to(g_real, DOWN)
+        statements = VGroup(g_real, f_rational)
+        brace = Brace(Line(LEFT * 0.6, RIGHT * 0.6)).rotate(PI/2).next_to(statements, RIGHT)
+        imply = Tex(r"$\Longrightarrow$", font_size = fs).next_to(statements, DOWN + LEFT).shift([0.5, -0.4, 0])
+        g_rational = Tex(r"g tiene coeficientes racionales", font_size = fs, color=ORANGE).next_to(imply, RIGHT)
+        self.play(FadeOut(g_real_proof_1_1),
+                  FadeOut(g_real_proof_3_2))
+        self.play(Write(f_rational),
                 ApplyWave(pol_2_2),
                 pol_2_2.animate.set_color(ORANGE))
+        self.play(pol_2_2.animate.set_color(WHITE),
+            Write(brace), Write(imply), Write(g_rational))
+        self.wait(2)
+
+        self.play(FadeOut(g_real), FadeOut(f_rational), FadeOut(brace), FadeOut(imply))
+        self.play(g_rational.animate.shift([-0.5, 1.5, 0]))
         self.wait()
 
-        
+        ipi_g = Tex(r"$i\pi$ es raíz de $g$", font_size = fs, color=WHITE).next_to(g_rational, DOWN * 3)
+        ipi_g_proof_1_1 = MathTex(r"g(i\pi) ")
+        ipi_g_proof_1_2 = MathTex(r"= f(i^2\pi)f(-i^2\pi)", font_size = fs).next_to(ipi_g_proof_1_1, RIGHT)
+        ipi_g_proof_1 = VGroup(ipi_g_proof_1_1, ipi_g_proof_1_2).next_to(ipi_g, DOWN)
+        ipi_g_proof_2 = MathTex(r"= f(-(\sqrt{-1})^2\pi)f(-(\sqrt{-1})^2\pi)", font_size=fs).next_to(ipi_g_proof_1, DOWN)
+        ipi_g_proof_3 = MathTex(r"= f(-\pi)f(\pi)", font_size=fs).next_to(ipi_g_proof_2, DOWN)
+        ipi_g_proof_4 = MathTex(r"= 0", font_size=fs).next_to(ipi_g_proof_3, DOWN)
+
+        self.play(Write(ipi_g))
+        self.play(Write(ipi_g_proof_1), run_time=2)
+        self.play(Write(ipi_g_proof_2), run_time=2)
+        self.play(Write(ipi_g_proof_3), run_time=2)
+        self.play(Write(ipi_g_proof_4), run_time=2)
+        self.wait()
+
+        self.play(
+            FadeOut(ipi_g_proof_1_2),
+            FadeOut(ipi_g_proof_2),
+            FadeOut(ipi_g_proof_3),
+            ipi_g_proof_1_1.animate.next_to(ipi_g, DOWN).shift([-0.4, 0, 0]),
+            ipi_g_proof_4.animate.next_to(ipi_g, DOWN).shift([0.6, -0.05, 0])
+        )
+        self.play(ipi_g.animate.set_color(ORANGE))
+        self.play(ipi_g.animate.shift([0, 0.5, 0]), FadeOut(ipi_g_proof_1_1), FadeOut(ipi_g_proof_4))
+        self.wait()
+
+        g_no_nulo = Tex(r"g no es el polinomio nulo", font_size = fs, color = ORANGE).next_to(ipi_g, DOWN)
+        statements = VGroup(g_rational, ipi_g, g_no_nulo)
+        brace = Brace(Line(LEFT * 0.9, RIGHT * 0.9)).rotate(PI/2).next_to(statements, RIGHT)
+        imply = Tex(r"$\Longrightarrow$", font_size = fs).next_to(statements, DOWN + LEFT).shift([1.6, -0.4, 0])
+        ipi_alg = Tex(r"$i\pi$ es algebraico", font_size = fs, color=ORANGE).next_to(imply, RIGHT)
+
+
+        self.play(Write(g_no_nulo))
+        self.play(Write(brace), Write(imply), Write(ipi_alg))
+        self.wait()
+
+        self.play(FadeOut(g_rational), FadeOut(ipi_g), FadeOut(g_no_nulo), FadeOut(brace), FadeOut(imply), FadeOut(g))
+        fact_1_copy = fact_1.copy()
+
+        self.play(fact_1_copy.animate.next_to(ipi_alg, UP * 1.2))
+        self.play(fact_1_copy.animate.scale(1.4))
+        e_pii = Tex(r"$e^{i\pi}$ es algebraico", font_size = fs*1.5, color = BLUE).move_to(ipi_alg.get_center())
+        self.play(Transform(ipi_alg, e_pii), FadeOut(fact_1_copy))
+        self.wait(2)
+        fact_2_copy = fact_2.copy()
+        self.play(fact_2_copy.animate.next_to(ipi_alg, UP * 1.2))
+        self.play(fact_2_copy.animate.scale(1.6))
+        cont = Tex(r"Contradicción!", font_size = fs*1.5, color = RED).move_to(ipi_alg.get_center())
+        self.play(Transform(ipi_alg, cont), FadeOut(fact_2_copy))
+        self.wait(2)
